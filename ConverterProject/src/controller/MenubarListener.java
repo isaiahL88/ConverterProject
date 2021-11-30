4126f6525c39dfb90470c8269a5622037745a055
chunkitchung <38967163+chunkitchung@users.noreply.github.com>
2021-11-30 01:49:28 -0500

    Update MenubarListener.java

--- ConverterProject/src/controller/MenubarListener.java
+++ ConverterProject/src/controller/MenubarListener.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class MenubarListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import view.DisplayPanel;
+
+public class MenubarListener implements ActionListener{
+	
+	private final DisplayPanel displayPanel;
+
+	public MenubarListener(DisplayPanel panel) {
+		displayPanel = panel;
+	}
+	
+	//@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
