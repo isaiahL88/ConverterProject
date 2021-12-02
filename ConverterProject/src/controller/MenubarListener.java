9fc5a02b50729d00b215c07e4f7f0f3e3b642f62
chunkitchung <38967163+chunkitchung@users.noreply.github.com>
2021-12-02 15:15:33 -0500

    Update MenubarListener.java

--- ConverterProject/src/controller/MenubarListener.java
+++ ConverterProject/src/controller/MenubarListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import view.DisplayPanel;
+import javax.swing.JOptionPane;
 
 public class MenubarListener implements ActionListener{
 	
@@ -16,6 +17,12 @@ public class MenubarListener implements ActionListener{
 	//@Override
 	public void actionPerformed(ActionEvent e) {
 		String value = this.displayPanel.CentimetersConversionArea.getText();
+		
+		try {
+			double centimetes = Double.parseDouble(value);
+		} catch (NumberFormatException error) {
+			JOptionPane.showMessageDialog(null, "please enter a number", "error", JOptionPane.INFORMATION_MESSAGE);
+		}
 		double centimetes = Double.parseDouble(value);
 		this.displayPanel.valueModel.setValue(centimetes);
 	}
