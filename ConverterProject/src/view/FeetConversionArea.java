00b025935b06a363c4ad724ec51ea9136385bdae
chunkitchung <38967163+chunkitchung@users.noreply.github.com>
2021-12-02 17:08:08 -0500

    Update FeetConversionArea.java

--- ConverterProject/src/view/FeetConversionArea.java
+++ ConverterProject/src/view/FeetConversionArea.java
@@ -9,7 +9,11 @@ public class FeetConversionArea extends JTextArea implements Observer{
 	public FeetConversionArea(String s, int x, int y) {
 		super(s,x,y);
 	}
-	
+	/*
+	* Preconditions: parameter value is a feet value
+  	* Postconditions: parameter value is converted from a feet value to a meter value 
+ 	* @param value the value needed to convert to feet
+	*/
 	public void updateVal(double value) {
 		value=value*0.0328084;
 		DecimalFormat df = new DecimalFormat("#.####");
