4b298e5b721a450bd3c3271785468687069951f7
leongli <79017154+leongli@users.noreply.github.com>
2021-12-02 15:59:33 -0500

    Update ValueToConvert.java

--- ConverterProject/src/model/ValueToConvert.java
+++ ConverterProject/src/model/ValueToConvert.java
@@ -1,24 +1,42 @@
 package model;
 
+import java.util.ArrayList;
+//import java.util.Observer;
+
+import view.DisplayPanel;
+import view.Observer;
+
 public class ValueToConvert {
 	private double value;
-	
-	public void setValue(double value) {
-		this.value = value;
-		System.out.println("@@@@@NEW VALUE IN MODEL: " + value);
+	//create list of observers
+	private ArrayList<Observer> listOfObservers;
+	private final DisplayPanel displayPanel;
+
+	public ValueToConvert(DisplayPanel panel) {
+		displayPanel = panel;
+		listOfObservers=new ArrayList<Observer>();
 	}
-	
 	public double getValue() {
 		return this.value;
 	}
-	
-	public void CmToFeet() {
-		double valueinfeet = value /2.54;
-		System.out.println("@@@@@NEW VALUE IN feet: " + valueinfeet);
+	public void setValue(double value) {
+		this.value = value;
+//		System.out.println("@@@@@NEW VALUE IN MODEL: " + value);
+		notifyObserver();
+	}
+	//mutator for observer: attach detach notify
+	public void addObserver(Observer o) {
+		listOfObservers.add(o);
+	}
+	public void removeObserver(Observer o) {
+		listOfObservers.remove(o);
 	}
 	
-	public void CmToMeter() {
-		double valueinmeter = value /100;
-		System.out.println("@@@@@NEW VALUE IN meter: " + valueinmeter);
+	public void notifyObserver() {
+		for (Observer o: listOfObservers)
+			o.updateVal(value);
+//		this.displayPanel.FCA.updateVal(value);
+//		this.displayPanel.MCA.updateVal(value);
 	}
+	
 }
