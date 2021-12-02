f67c5a9a2936532dcc15382bf4bd14754afe519e
leongli <79017154+leongli@users.noreply.github.com>
2021-12-02 15:58:00 -0500

    Create MeterConversionArea.java

---
+++ ConverterProject/src/view/MeterConversionArea.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.text.DecimalFormat;
+
+import javax.swing.*;
+
+public class MeterConversionArea extends JTextArea implements Observer{
+	
+	public MeterConversionArea(String s, int x, int y) {
+		super(s,x,y);
+	}
+	
+	public void updateVal(double value) {
+		
+		value=value*0.01;
+		DecimalFormat df = new DecimalFormat("#.####");
+		value=Double.parseDouble(df.format(value));
+		this.setText(""+value+" m");
+	}
+}
