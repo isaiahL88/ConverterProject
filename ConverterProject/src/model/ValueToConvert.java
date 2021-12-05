be141dce76b4b346f92199179d5db67294b13adf
42ing <43578896+42ing@users.noreply.github.com>
2021-12-05 18:58:50 -0500

    Update ValueToConvert.java

--- ConverterProject/src/model/ValueToConvert.java
+++ ConverterProject/src/model/ValueToConvert.java
@@ -12,28 +12,47 @@ public class ValueToConvert {
 	private ArrayList<Observer> listOfObservers;
 	//private final DisplayPanel displayPanel;
 
+	/**
+	* create a Arraylist for Observer to register
+	*/
 	public ValueToConvert() {
 		//displayPanel = panel;
 		listOfObservers=new ArrayList<Observer>();
 	}
+	
+	/**
+	* get the value of input
+	* postcondition: get the value of input from CentimetersConversionArea
+	*/
 	public double getValue() {
 		return this.value;
 	}
+	/**
+	* update the value of input
+	* postcondition: the value encapsulated in is updated and send a 
+	* singal to Observer
+	* @param value A double number that get from CentimetersConversionArea
+	*/
+	
 	public void setValue(double value) {
 		this.value = value;
 		notifyObserver();
 	}
 	//mutator for observer: attach detach notify
-	//Adds an observer to the ArrayList of observers for this object
+	/**
+	*Adds an observer to the ArrayList of observers for this object
+	*/
+	
 	public void addObserver(Observer o) {
 		listOfObservers.add(o);
 	}
-	
-	//remove an observer to the ArrayList of observers for this object
+	/**
+	*remove an observer to the ArrayList of observers for this object
+	*/
 	public void removeObserver(Observer o) {
 		listOfObservers.remove(o);
 	}
-	/*
+	/**
 	*update value to all observer in the list
 	*Each observer has its update method called
 	*precondition: value is changed and this method is getting called
