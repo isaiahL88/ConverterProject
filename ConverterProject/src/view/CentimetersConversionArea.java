35480e86e42b00fd439fd9151b42a707a5c622f0
42ing <43578896+42ing@users.noreply.github.com>
2021-12-05 18:47:07 -0500

    Update CentimetersConversionArea.java

--- ConverterProject/src/view/CentimetersConversionArea.java
+++ ConverterProject/src/view/CentimetersConversionArea.java
@@ -1,10 +1,20 @@
 package view;
 import javax.swing.*;
 public class CentimetersConversionArea extends JTextArea{
+	
+	/*
+	* constructor for JtextArea from DisplayPanel
+ 	* @param s A string contain preset text in MeterConversionArea
+	* @param x A integer to constructs JTextArea with the specified number of rows 
+	* @param y A integer to constructs JTextArea with the specified number of columns 
+	*/
 	public CentimetersConversionArea(String s, int x, int y) {
 		super(s,x,y);
 	}
 	
+	/** Gets the text of the input.
+ 	* @return A string of input in the JTextArea
+	*/
 	public String getValue() {
 		return this.getText();
 	}
