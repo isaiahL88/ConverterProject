6d6f9f48a5bb8b1eef48bf59cb2199ff47fd62da
42ing <43578896+42ing@users.noreply.github.com>
2021-12-05 19:23:46 -0500

    Update ConverterApp.java

--- ConverterProject/src/main/ConverterApp.java
+++ ConverterProject/src/main/ConverterApp.java
@@ -9,7 +9,14 @@ import controller.MenubarListener;
 import view.ConverterMenuBar;
 import view.DisplayPanel;
 
+/** 
+* the main of ConverterApp
+*/
 public class ConverterApp {
+	/** 
+	* Our main method. to create an App that convert input in centimeter into feet and meter
+	* @param args The command line arguments.
+	*/
 
 	public static void main(String[] args) {
 
