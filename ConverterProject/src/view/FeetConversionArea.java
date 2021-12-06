43ef3283bccd032a362243c5968cc791f36ca350
42ing <43578896+42ing@users.noreply.github.com>
2021-12-05 20:05:41 -0500

    Update FeetConversionArea.java

--- ConverterProject/src/view/FeetConversionArea.java
+++ ConverterProject/src/view/FeetConversionArea.java
@@ -5,11 +5,11 @@ import java.text.DecimalFormat;
 import javax.swing.*;
 
 	
-	/*
+	/**
 	* The class invariant is that there is always a value to convert
 	*/
 public class FeetConversionArea extends JTextArea implements Observer{
-	 /*
+	 /**
 	* constructor for JtextArea from DisplayPanel
  	* @param s A string contain preset text in MeterConversionArea
 	* @param x A integer to constructs JTextArea with the specified number of rows 
@@ -18,7 +18,7 @@ public class FeetConversionArea extends JTextArea implements Observer{
 	public FeetConversionArea(String s, int x, int y) {
 		super(s,x,y);
 	}
-	/*
+	/**
 	* Preconditions: parameter value is a centimeter value
   	* Postconditions: parameter value is converted from a centimeter value to a feet value 
  	* @param value the value needed to convert to feet
